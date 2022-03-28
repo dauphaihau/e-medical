8e22d9d60f40dcb33c06606960d267979bee9d7f
QuocVu <quocva@edoctor.vn>
2022-03-28 09:44:38 +0700

    fix

--- pages/dang-nhap.js
+++ pages/dang-nhap.js
@@ -13,7 +13,7 @@ import Button from "@components/button";
 import logo2 from "../public/images/logo2.png";
 import {accountService} from '@services';
 
-const phoneRegExp = /(([03+[2-9]|05+[6|8|9]|07+[0|6|7|8|9]|08+[1-9]|09+[1-4|6-9]]){3})+[0-9]{7}\b/
+const phoneRegExp = /^[+|0]?\d{12}$/;
 const pinRegExp = /^\d{4}$/
 
 const loginSchema = Yup.object().shape({
